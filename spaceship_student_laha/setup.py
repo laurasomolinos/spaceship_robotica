3edc6dd1f3aa13082add8207c33f9b3581b399ff
laurasomolinos <laura.somolinos@alumnos.upm.es>
2026-05-14 12:27:15 +0200

    subir carpeta inicial

---
+++ spaceship_student_laha/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'spaceship_student_laha'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='laura',
+    maintainer_email='laura.somolinos@alumnos.upm.es',
+    description='Paquete propio del grupo para el controlador de la nave espacial',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
